c77e1fff385ad1129f534eddc15b39717c89e4a9
A-STAR <andreysuperstar@gmail.com>
2018-04-09 20:17:59 +0300

    refactor(currency): rewrite service and template to Observable

--- src/app/currency/shared/currency.service.ts
+++ src/app/currency/shared/currency.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+
+const URI = 'wss://api.bitfinex.com/ws';
 
 @Injectable()
 export class CurrencyService {
 
   private socket: WebSocket;
-  prices$ = new BehaviorSubject<number[]>([]);
+  private prices: number[] = [];
+  prices$: Observable<number[]>;
 
   constructor() { }
 
   initSocket() {
-    this.socket = new WebSocket('wss://api.bitfinex.com/ws');
+    this.socket = new WebSocket(URI);
 
     const event = {
       event: 'subscribe',
@@ -20,10 +24,10 @@ export class CurrencyService {
 
     this.socket.onopen = () => this.socket.send(JSON.stringify(event));
 
-    this.socketMessage();
+    this.prices$ = Observable.create(this.socketMessage.bind(this));
   }
 
-  private socketMessage() {
+  private socketMessage(observer: Observer<number[]>) {
 
     this.socket.onmessage = (event: MessageEvent) => {
 
@@ -32,10 +36,9 @@ export class CurrencyService {
       if (data[7]) {
         const lastPrice: number = data[7];
 
-        const prices = this.prices$.value;
-        prices.unshift(lastPrice);
+        this.prices.unshift(lastPrice);
 
-        this.prices$.next(prices);
+        observer.next(this.prices);
       }
 
     };
