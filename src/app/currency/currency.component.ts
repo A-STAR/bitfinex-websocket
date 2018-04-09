f4de7b1ae3e855f225c07feea5c021892d7d5191
A-STAR <andreysuperstar@gmail.com>
2018-04-09 04:01:46 +0300

    feat(currency): add Bitfinex WebSocket connection, styles and markup

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrencyComponent implements OnInit {
 
+  socket: WebSocket;
+  prices: number[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.initSocket();
+
+    this.socketMessage();
+  }
+
+  private initSocket() {
+    this.socket = new WebSocket('wss://api.bitfinex.com/ws');
+
+    const event = {
+      event: 'subscribe',
+      channel: 'ticker',
+      pair: 'BTCUSD'
+    };
+
+    this.socket.onopen = () => this.socket.send(JSON.stringify(event));
+  }
+
+  private socketMessage() {
+
+    this.socket.onmessage = (event: MessageEvent) => {
+
+      const data: any = JSON.parse(event.data);
+
+      if (data[7]) {
+        const lastPrice: number = data[7];
+
+        this.prices.unshift(lastPrice);
+      }
+
+    };
+
   }
 
 }
